1ff5dfbf88ca104687c2682519f26e02cff53fb9
Daniel Vilas Perulan <daniel.vilas@gmail.com>
2024-10-07 18:56:03 +0200

    First Basic Control

---
+++ firmware/include/pins_cfg.h
@@ -0,0 +1,29 @@
+
+#ifndef DDT_PINS_CFG
+#define DDT_PINS_CFG
+
+#include <Arduino.h>
+
+
+#define COL0_RED PA0
+#define COL1_BLAC_L PA1
+#define ROW0_YELLOW PA4
+#define ROW1_ORANGE PA8
+#define ROW2_BLUE PB0
+#define ROW3_BLACK PA12
+
+#define WS2812_LED PB1
+
+
+/*
+PA_00 Col0 Rojo
+PA_01 Col1 Negro L
+PA_04 Row0 Amarillo
+PA_08 Row1 Naranja
+PA_11 Row3 Negro (Llave)
+PB_00 WS2812 Marron
+PB_01 Row2 azul
+
+ */
+
+#endif
